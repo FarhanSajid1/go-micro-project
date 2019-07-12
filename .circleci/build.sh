110fd803b348d2cf915d9d8d7dc3841149d2fc90
Farhan Sajid <FarhanSajid7861@gmail.com>
2019-07-11 20:04:20 -0400

    added docker login

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,5 +3,7 @@
 docker build -t farhansajid2/user-service:$CIRCLE_SHA1 ./user-service/Dockerfile .
 docker build -t farhansajid2/email-service:$CIRCLE_SHA1 ./email-service/Dockerfile .
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+
 docker push farhansajid2/email-service:$CIRCLE_SHA1
 docker push farhansajid2/user-service:$CIRCLE_SHA1
